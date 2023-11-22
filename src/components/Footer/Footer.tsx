b53bd66445b283dbe8975b435c88e0d2505f5db1
Vale <valeriacesarini@gmail.com>
2023-11-22 18:00:50 -0300

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,24 @@
-import { Container, Row } from "react-bootstrap"
+import { BsBraces, BsFacebook, BsInstagram, BsTwitterX, BsWhatsapp } from "react-icons/bs"
 
 const Footer = () => {
   return (
-    <Container>
-    <Row className='d-flex justify-content-center'>
-      <h1>footer</h1>
-    </Row>
-  </Container>
+    <div className="container">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
+          <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+            <BsBraces/>
+          </a>
+          <span className="mb-3 mb-md-0 text-muted">VC PROYECTO © 2023</span>
+        </div>
+
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li className="ms-3"><a className="text-muted" href="#"><BsWhatsapp/></a></li>
+          <li className="ms-3"><a className="text-muted" href="#"><BsInstagram/></a></li>
+          <li className="ms-3"><a className="text-muted" href="#"><BsFacebook/></a></li>
+          <li className="ms-3"><a className="text-muted" href="#"><BsTwitterX/></a></li>
+        </ul>
+      </footer>
+    </div>
   )
 }
 

c684f38e2b1bc7b562801341c9d373ce3fa7600d
Vale <valeriacesarini@gmail.com>
2023-11-22 21:28:07 -0300

    Update Nosotros.tsx

--- src/pages/Nosotros.tsx
+++ src/pages/Nosotros.tsx
@@ -1,5 +1,6 @@
 import { Container, Row } from "react-bootstrap"
 import HeroImage from "../components/HeroImage/HeroImage"
+import { BsPersonRaisedHand } from "react-icons/bs"
 
 const Nosotros = () => {
   return (
@@ -7,7 +8,17 @@ const Nosotros = () => {
       <HeroImage url="https://fakeimg.pl/1920x300/00a8a7/ffffff?text=Nosotros" />
       <Container className="mt-5">
         <Row className='d-flex justify-content-center'>
-          <h1>Nosotros</h1>
+        <div className="px-4 py-5 text-center">
+          <BsPersonRaisedHand style={{ fontSize: '50px'}} />
+          <h1 className="display-5 fw-bold mt-4">VC PROYECTO</h1>
+          <div className="col-lg-6 mx-auto">
+            <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, opular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+            {/* <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+              <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+              <button type="button" className="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+            </div> */}
+          </div>
+        </div>
         </Row>
       </Container>
     </>

95fad21bf59db41a96aebcbf6323440b6ef0138f
Vale <valeriacesarini@gmail.com>
2023-11-24 18:01:13 -0300

    col responsive

--- src/components/CategoriasTareas/CategoriasTareas.tsx
+++ src/components/CategoriasTareas/CategoriasTareas.tsx
@@ -1,4 +1,4 @@
-import { Card, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import { Task } from "../../types/Task";
 import { Link } from "react-router-dom";
 
@@ -18,24 +18,24 @@ const CategoriasTareas = ({tasks} : {tasks: Task[]}) => {
         <h5>{categoria}</h5>
       </Row>
 
-      <Row className='d-flex justify-content-center gap-3'>
+      <Row className='d-flex justify-content-center'>
       {tasks.filter(tasks => tasks.estado === categoria.toUpperCase())
         .map(task => (
-
-        <Card style={{ width: '18rem', padding: '0', marginBottom: '20px' }} key={task.id}>
+        <Col xs={6} md={4} lg={3}>
+        <Card key={task.id}>
           <Card.Img variant="top" src={task.imagen} className="card-img" />
-          <Card.Body>
-            <Card.Title className="titulo-ellipsis">{task.titulo}</Card.Title>
+          <Card.Body style={{ height: '260px'}}>
+            <Card.Title className="titulo-ellipsis"><b>{task.titulo}</b></Card.Title>
             <Card.Text className="multiline-ellipsis">
               {task.descripcion}
             </Card.Text>
-            <Card.Text>
-              <b>Estado de tarea: {task.estado}</b>
+            <Card.Text className="mt-4">
+              <b>La tarea está {task.estado}</b>
             </Card.Text>
             <Link to={`/detalle/${task.id}`} onClick={scrollToTop} className="btn btn-success">DETALLE TAREA</Link>
           </Card.Body>
         </Card>
-        
+        </Col>
       ))}
       </Row>
     </Container>
